85932f80e53634599dc5585fd4115100f6b8fd9d
sandrohmt <sandrohmt@gmail.com>
2024-11-26 14:14:38 -0300

    feat: create GroupRepository

---
+++ src/main/java/com/sandrohenrique/secret_santa/repositories/GroupRepository.java
@@ -0,0 +1,13 @@
+package com.sandrohenrique.secret_santa.repositories;
+
+import com.sandrohenrique.secret_santa.domain.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Optional<Group> findGroupByName(String name);
+    Optional<Group> findGroupById(Long id);
+}
