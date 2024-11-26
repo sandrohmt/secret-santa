47559ede7f58246cddc3ac5a2a7566b9327de063
sandrohmt <sandrohmt@gmail.com>
2024-11-26 14:10:57 -0300

    feat: create Friend entity

---
+++ src/main/java/com/sandrohenrique/secret_santa/domain/Friend.java
@@ -0,0 +1,36 @@
+package com.sandrohenrique.secret_santa.domain;
+
+import com.sandrohenrique.secret_santa.dtos.FriendDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @ElementCollection
+    private List<String> wishlist;
+
+    @OneToOne
+    private Friend drawnFriend;
+
+    public Friend(FriendDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.wishlist = data.wishlist();
+    }
+}
