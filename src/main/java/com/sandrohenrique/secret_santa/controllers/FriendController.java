a89c5879a1248fee9478df7e52787354f174c596
sandrohmt <sandrohmt@gmail.com>
2024-11-26 14:11:11 -0300

    feat: create FriendController

---
+++ src/main/java/com/sandrohenrique/secret_santa/controllers/FriendController.java
@@ -0,0 +1,26 @@
+package com.sandrohenrique.secret_santa.controllers;
+
+import com.sandrohenrique.secret_santa.domain.Friend;
+import com.sandrohenrique.secret_santa.dtos.FriendDTO;
+import com.sandrohenrique.secret_santa.services.FriendService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/friends")
+@RequiredArgsConstructor
+public class FriendController {
+
+    private final FriendService friendService;
+
+    @PostMapping
+    public ResponseEntity<Friend> createFriend(@RequestBody FriendDTO friend) {
+        Friend newFriend = friendService.createFriend(friend);
+        return new ResponseEntity<>(newFriend, HttpStatus.CREATED);
+    }
+}
