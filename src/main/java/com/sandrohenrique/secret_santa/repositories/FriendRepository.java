575e6705b6b0dd859464c3bf280a69472f462eb2
sandrohmt <sandrohmt@gmail.com>
2024-11-29 16:14:04 -0300

    feat: FriendRepository findAllById

--- src/main/java/com/sandrohenrique/secret_santa/repositories/FriendRepository.java
+++ src/main/java/com/sandrohenrique/secret_santa/repositories/FriendRepository.java
@@ -4,10 +4,12 @@ import com.sandrohenrique.secret_santa.domain.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
      Optional<Friend> findFriendById(Long id);
+     List<Friend> findAllById(Iterable<Long> id);
 
 }
