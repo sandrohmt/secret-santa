563920a554d85f649abe3b0928747ee23186fa8c
sandrohmt <sandrohmt@gmail.com>
2024-12-02 14:21:33 -0300

    feat: add a method drawnFriends on GroupService

--- src/main/java/com/sandrohenrique/secret_santa/services/GroupService.java
+++ src/main/java/com/sandrohenrique/secret_santa/services/GroupService.java
@@ -2,7 +2,6 @@ package com.sandrohenrique.secret_santa.services;
 
 import com.sandrohenrique.secret_santa.domain.Friend;
 import com.sandrohenrique.secret_santa.domain.Group;
-import com.sandrohenrique.secret_santa.dtos.AddFriendDTO;
 import com.sandrohenrique.secret_santa.dtos.AddFriendsDTO;
 import com.sandrohenrique.secret_santa.dtos.GroupDTO;
 import com.sandrohenrique.secret_santa.dtos.GroupWithFriendsDTO;
@@ -11,8 +10,7 @@ import com.sandrohenrique.secret_santa.repositories.GroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -46,7 +44,7 @@ public class GroupService {
 
 
     public GroupWithFriendsDTO findGroupByName(String name) {
-        Group group = this.groupRepository.findGroupByName(name).orElseThrow(RuntimeException::new);// Criar uma exceção pra isso
+        Group group = this.groupRepository.findGroupByName(name).orElseThrow(RuntimeException::new); // Criar uma exceção
 
         List<Friend> friends = friendRepository.findAllById(group.getFriendIds());
 
@@ -55,7 +53,7 @@ public class GroupService {
     }
 
     public void addFriendsById(AddFriendsDTO data) {
-        Optional<Group> groupOpt = groupRepository.findGroupById(data.groupId());
+        Optional<Group> groupOpt = groupRepository.findGroupById(data.groupId()); // Talvez fazer igual aos outros métodos orElseThrow
 
         for (Long friendId : data.friendIds()) {
             Optional<Friend> friendOpt = friendRepository.findFriendById(friendId);
@@ -69,20 +67,53 @@ public class GroupService {
                 throw new RuntimeException("Usuário já está nesse grupo!"); // Criar uma exceção
             }
             group.getFriendIds().add(friendId);
+            group.setDrawn(false);
             saveGroup(group);
+        }
+    }
+
+    public void drawFriends(Long id) {
+        Group group = groupRepository.findGroupById(id).orElseThrow(RuntimeException::new); // Criar uma exceção
+
+        if (group.isDrawn()) {
+            throw new RuntimeException("Grupo já foi sorteado!"); // Criar uma exceção
+        }
+        List<Long> friendIds = group.getFriendIds();
+
+        List<Friend> friends = friendRepository.findAllById(group.getFriendIds());
+
+        if (friends.size() < 2) {
+            throw new RuntimeException("É necessário pelo menos 2 amigos para realizar o sorteio!"); // Criar uma exceção
+        }
+
+        LinkedList<Long> availableFriends = new LinkedList<>(friendIds);
+        Collections.shuffle(availableFriends);
+
+
+        for (Friend friend : friends) {
+            Iterator<Long> iterator = availableFriends.iterator();
+            while(iterator.hasNext()) {
+                Long drawnId = iterator.next();
+                if (!drawnId.equals(friend.getId())) {
+                    friend.setDrawnFriendId(drawnId);
+                    iterator.remove();
+                    break;
+                }
+            }
 
         }
 
+        friendRepository.saveAll(friends);
 
-
+        group.setDrawn(true);
+        saveGroup(group);
     }
-
 }
 
+// Temos um problema... se um amigo participa de dois sorteios diferentes ele nao consegue manter o drawnFriend dos dois, mantém do ultimo.
+// Fazer com que o sorteio seja cíclico
 // Encontrar amigo do grupo pelo id, entao precisa do id do grupo e do id do amigo tambem
 // Fazer um metodo Post para a pessoa saber quem ela tirou
-// Tentar adicionar varios amigos de uma vez no grupo
-// Fazer toda a regra de negocio de sortear um amigo pra cada amigo (tentar fazer com que o amigo oculto seja dinamico, que um par de pessoas nao se tire)
 // Fazer todas as exceções necessarias
 // Lançar exceção com nome de grupo repetido
 // delete update friend
@@ -91,3 +122,4 @@ public class GroupService {
 // talvez disparar emails
 // segurança?
 // testes
+// Tentar fazer com lista encadeada
