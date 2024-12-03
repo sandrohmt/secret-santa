eb43f66dafb40cdbc8cb0c19e402cd7ca1738f49
sandrohmt <sandrohmt@gmail.com>
2024-12-03 15:57:03 -0300

    fix: add more details when sending an email on EmailService

--- src/main/java/com/sandrohenrique/secret_santa/services/EmailService.java
+++ src/main/java/com/sandrohenrique/secret_santa/services/EmailService.java
@@ -6,6 +6,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class EmailService {
@@ -14,13 +16,24 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String sender;
 
-    public String sendTextEmail(String recipient, String subject, String message) {
+    public String sendTextEmail(String recipientEmail, String recipientName, String subject, String drawnFriendName, List<String> drawnFriendWishlist) {
 
         try {
             SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
             simpleMailMessage.setFrom(sender);
-            simpleMailMessage.setTo(recipient);
+            simpleMailMessage.setTo(recipientEmail);
             simpleMailMessage.setSubject(subject);
+            String message = """
+                    Olá, %s!
+                    
+                    Você foi convidado para participar de um amigo oculto!
+                    Seu amigo oculto é: %s
+                    
+                    Segue a lista de desejos dele:
+                    - %s
+                    
+                    Divirta-se e boas festas!
+                    """.formatted(recipientName, drawnFriendName, drawnFriendWishlist);
             simpleMailMessage.setText(message);
             javaMailSender.send(simpleMailMessage);
             return "Email enviado";
