c13d3cfee1ed156f0564a2e369ad979a6d580d51
sandrohmt <sandrohmt@gmail.com>
2024-12-03 02:06:53 -0300

    fix: add the email attribute for FriendDTO

--- src/main/java/com/sandrohenrique/secret_santa/dtos/FriendDTO.java
+++ src/main/java/com/sandrohenrique/secret_santa/dtos/FriendDTO.java
@@ -2,5 +2,5 @@ package com.sandrohenrique.secret_santa.dtos;
 
 import java.util.List;
 
-public record FriendDTO(String firstName, String lastName, List<String> wishlist) {
+public record FriendDTO(String firstName, String lastName, String email, List<String> wishlist) {
 }
