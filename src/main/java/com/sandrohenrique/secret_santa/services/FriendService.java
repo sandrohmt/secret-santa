9413815f5eb60937615376cc0f97c9a9e7cd4678
sandrohmt <sandrohmt@gmail.com>
2024-12-03 15:59:56 -0300

    fix: implement validation in FriendRepository to not leave two friends with the same email

--- src/main/java/com/sandrohenrique/secret_santa/services/FriendService.java
+++ src/main/java/com/sandrohenrique/secret_santa/services/FriendService.java
@@ -2,9 +2,12 @@ package com.sandrohenrique.secret_santa.services;
 
 import com.sandrohenrique.secret_santa.domain.Friend;
 import com.sandrohenrique.secret_santa.dtos.FriendDTO;
+import com.sandrohenrique.secret_santa.exceptions.UserAlreadyInGroupException;
 import com.sandrohenrique.secret_santa.repositories.FriendRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +21,12 @@ public class FriendService {
 
     public Friend createFriend(FriendDTO data) {
         Friend newFriend = new Friend(data);
+        List<Friend> friends = this.friendRepository.findAll();
+        for (Friend friend : friends) {
+            if (newFriend.getEmail().equals(friend.getEmail())) {
+                throw new UserAlreadyInGroupException("Usuário já cadastrado com esse email!"); // Talvez fazer isso de outra forma, porque ele precisa percorrer a lista inteira pra fazer essa verificação
+            }
+        }
         saveUser(newFriend);
         return newFriend;
     }
