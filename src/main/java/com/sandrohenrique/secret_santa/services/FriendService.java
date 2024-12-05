f475a17d9d244899082abde2fbc2186969847313
sandrohmt <sandrohmt@gmail.com>
2024-12-05 01:13:12 -0300

    feat: create saveAllUsers

--- src/main/java/com/sandrohenrique/secret_santa/services/FriendService.java
+++ src/main/java/com/sandrohenrique/secret_santa/services/FriendService.java
@@ -2,18 +2,26 @@ package com.sandrohenrique.secret_santa.services;
 
 import com.sandrohenrique.secret_santa.domain.Friend;
 import com.sandrohenrique.secret_santa.dtos.FriendDTO;
-import com.sandrohenrique.secret_santa.exceptions.UserAlreadyInGroupException;
+import com.sandrohenrique.secret_santa.dtos.GroupFriendDTO;
+import com.sandrohenrique.secret_santa.exceptions.EntityNotFoundException;
+import com.sandrohenrique.secret_santa.exceptions.FriendAlreadyInGroupException;
 import com.sandrohenrique.secret_santa.repositories.FriendRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class FriendService {
 
     private final FriendRepository friendRepository;
+//    private final GroupService groupService; Provavelmente mandar remover o amigo no groupService
+
+    public void saveAllUsers(List<Friend> friends) {
+        this.friendRepository.saveAll(friends);
+    }
 
     public void saveUser(Friend friend) {
         this.friendRepository.save(friend);
