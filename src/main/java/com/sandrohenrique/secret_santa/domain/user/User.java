db15f6db3a699d855781887e8b3c65eee7fd774b
sandrohmt <sandrohmt@gmail.com>
2024-12-07 15:48:16 -0300

    feat: create User constructor

--- src/main/java/com/sandrohenrique/secret_santa/domain/user/User.java
+++ src/main/java/com/sandrohenrique/secret_santa/domain/user/User.java
@@ -23,6 +23,12 @@ public class User implements UserDetails { // Vem de dentro do Spring Security e
     private String password;
     private UserRole role;
 
+    public User(String login, String password, UserRole role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
