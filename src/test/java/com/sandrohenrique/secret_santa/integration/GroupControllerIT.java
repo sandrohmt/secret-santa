292311707ddf7cc29b1afb7eb56e08a8354b5a81
sandrohmt <sandrohmt@gmail.com>
2025-01-15 04:21:23 -0300

    feat: created GroupControllerIT with test findGroupById_ReturnGroupWithStatus200_WhenSuccessful

---
+++ src/test/java/com/sandrohenrique/secret_santa/integration/GroupControllerIT.java
@@ -0,0 +1,45 @@
+package com.sandrohenrique.secret_santa.integration;
+
+import com.sandrohenrique.secret_santa.domain.Friend;
+import com.sandrohenrique.secret_santa.dtos.GroupWithFriendsDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+class GroupControllerIT {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @DisplayName("findGroupById returns a Group with status 200 when successful")
+    void findGroupById_ReturnGroupWithStatus200_WhenSuccessful() {
+        Friend friend1 = new Friend(1L, "Maria", "Silva", "mariasilva@gmail.com", List.of("Playstation 5", "Celular"), null);
+        Friend friend2 = new Friend(2L, "José", "Souza", "josesouza@gmail.com", List.of("Tablet", "Piano"), null);
+        List<Friend> friends = List.of(friend1, friend2);
+
+        Long groupId = 1L;
+        LocalDate eventDate = LocalDate.of(2024, 12, 20);
+        GroupWithFriendsDTO expectedDTO = new GroupWithFriendsDTO(groupId, "Amigo Secreto de Fim de Ano", "Rua das Flores, 123 - Salão de Festas", eventDate, 100F, friends);
+
+        ResponseEntity<GroupWithFriendsDTO> response = testRestTemplate.getForEntity("/groups/{id}", GroupWithFriendsDTO.class, groupId);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+
+    }
+}
