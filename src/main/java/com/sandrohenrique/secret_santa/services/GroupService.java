e332c7c0596e7383875961f9febc379762aad536
sandrohmt <sandrohmt@gmail.com>
2025-01-15 03:26:33 -0300

    todos

--- src/main/java/com/sandrohenrique/secret_santa/services/GroupService.java
+++ src/main/java/com/sandrohenrique/secret_santa/services/GroupService.java
@@ -153,11 +153,8 @@ public class GroupService {
     }
 }
 
-// Mudar a estrutura do banco de dados, acho que friend e group deveriam ser (1:n), grupo pode ter varios amigos e um amigo so pode pertencer a um grupo. ai coloca mais um atributo em friend que seria groupId como fk
-// Talvez colocar LocalDateTime em vez de LocalDate, pra dizer o horario do evento tambem
 // Criar um Service compartilhado e colocar os métodos de GroupService que injetam FriendService e colocar o deleteFriends la, ou só colocar o metodo de deleteFriend mesmo, e fazer os testes desse service novo
-// Talvez criar um classe com método pra criar amigos em vez de ficar criando dentro de cada teste
 // delete group
 // documentar com o swagger, procurar o github do MurilloMarquesSantos para essas e mais coisas
-// Pesquisar stateful e stateless, csrf, bean e migrations
-// Postar o projeto no github
+// Pesquisar stateful e stateless, csrf, bean e migrations, rever o video de spring security da Fernanda Kipper
+// Postar o projeto no github com readme tudo bonitinho
