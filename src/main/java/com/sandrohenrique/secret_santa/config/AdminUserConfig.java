0dded65689176af87e19c5d360d3656c606d71c7
Sandro <sandrohmt@gmail.com>
2025-04-03 22:56:06 -0300

    fix: fixing the AdminUserConfig constructor

--- src/main/java/com/sandrohenrique/secret_santa/config/AdminUserConfig.java
+++ src/main/java/com/sandrohenrique/secret_santa/config/AdminUserConfig.java
@@ -5,6 +5,7 @@ import com.sandrohenrique.secret_santa.domain.user.User;
 import com.sandrohenrique.secret_santa.repositories.RoleRepository;
 import com.sandrohenrique.secret_santa.repositories.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.Set;
 
 @Configuration
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class AdminUserConfig implements CommandLineRunner {
 
     private RoleRepository roleRepository;
