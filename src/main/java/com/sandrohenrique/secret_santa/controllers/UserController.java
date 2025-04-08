de3ae2be610a670936a56bfad12a64953c3fbd74
Sandro <sandrohmt@gmail.com>
2025-04-08 20:00:21 -0300

    feat: created UserController swagger documentation

--- src/main/java/com/sandrohenrique/secret_santa/controllers/UserController.java
+++ src/main/java/com/sandrohenrique/secret_santa/controllers/UserController.java
@@ -5,6 +5,9 @@ import com.sandrohenrique.secret_santa.domain.user.User;
 import com.sandrohenrique.secret_santa.dtos.CreateUserDTO;
 import com.sandrohenrique.secret_santa.repositories.RoleRepository;
 import com.sandrohenrique.secret_santa.repositories.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,6 +28,7 @@ import java.util.Set;
 @RestController
 @RequiredArgsConstructor
 @EnableMethodSecurity
+@Tag(name = "Users", description = "Endpoints for managing users registration and list users in the Secret Santa system.")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -33,12 +37,16 @@ public class UserController {
 
     @Transactional
     @PostMapping("register")
+    @Operation(summary = "Create a new user", description = "Register a new user with role user in the Secret Santa system.")
+    @ApiResponse(responseCode = "201", description = "Successfully create a new user.")
+    @ApiResponse(responseCode = "422", description = "User is already created.")
+    @ApiResponse(responseCode = "500", description = "Unexpected server error while creating the new user.")
     public ResponseEntity<Void> createUser(@RequestBody  CreateUserDTO data) {
         Role userRole = roleRepository.findByName(Role.Values.USER.name());
 
         Optional<User> userFromDB = userRepository.findByLogin(data.login());
         if (userFromDB.isPresent()) {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "User already exists with this login.");
         }
 
         var user = new User();
@@ -47,10 +55,13 @@ public class UserController {
         user.setRoles(Set.of(userRole));
         userRepository.save(user);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("users")
+    @Operation(summary = "List all users", description = "Returns a list of all users registered in the Secret Santa system.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of users.")
+    @ApiResponse(responseCode = "500", description = "Unexpected server error while listing all users.")
     @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public ResponseEntity<List<User>> listUsers() {
         var users = userRepository.findAll();
