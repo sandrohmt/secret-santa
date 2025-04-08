a3c71acb28ff5b1d78ed1dc27f8d25a174ff8aff
Sandro <sandrohmt@gmail.com>
2025-04-08 20:46:10 -0300

    feat: created swagger documentation for ExceptionDTO

--- src/main/java/com/sandrohenrique/secret_santa/dtos/ExceptionDTO.java
+++ src/main/java/com/sandrohenrique/secret_santa/dtos/ExceptionDTO.java
@@ -1,4 +1,12 @@
 package com.sandrohenrique.secret_santa.dtos;
 
-public record ExceptionDTO(String message, String statusCode) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "DTO used to handle an exception.")
+public record ExceptionDTO(
+        @Schema(description = "Description of the exception's message", example = "Invalid login or password!")
+        String message,
+
+        @Schema(description = "Exception's HTTP status code as a String", example = "200")
+        String statusCode) {
 }
