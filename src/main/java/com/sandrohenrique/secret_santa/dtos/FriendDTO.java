108d7e7a9fa44ff534e57f2c2eb2c3739482bbae
sandrohmt <sandrohmt@gmail.com>
2025-04-08 23:34:29 -0300

    feat: created swagger documentation for FriendDTO

--- src/main/java/com/sandrohenrique/secret_santa/dtos/FriendDTO.java
+++ src/main/java/com/sandrohenrique/secret_santa/dtos/FriendDTO.java
@@ -1,6 +1,21 @@
 package com.sandrohenrique.secret_santa.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
-public record FriendDTO(String firstName, String lastName, String email, List<String> wishlist) {
+@Schema(description = "DTO used to create a new friend in the Secret Santa system.")
+public record FriendDTO(
+
+        @Schema(description = "Friend's name", example = "Sandro")
+        String firstName,
+
+        @Schema(description = "Friend's last name", example = "Teixeira")
+        String lastName,
+
+        @Schema(description = "Friend's email", example = "sandro@example.com")
+        String email,
+
+        @Schema(description = "Friend's wishlist", example = "[\"Tv\", \"Playstation 5\", \"Computer\"]")
+        List<String> wishlist) {
 }
