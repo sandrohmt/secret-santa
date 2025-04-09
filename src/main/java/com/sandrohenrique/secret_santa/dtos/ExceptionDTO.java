d90c1707c6cd05c8da341233d130c6f5e9253e71
sandrohmt <sandrohmt@gmail.com>
2025-04-08 23:35:13 -0300

    fix: fixing some detail in swagger documentation for ExceptionDTO

--- src/main/java/com/sandrohenrique/secret_santa/dtos/ExceptionDTO.java
+++ src/main/java/com/sandrohenrique/secret_santa/dtos/ExceptionDTO.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "DTO used to handle an exception.")
 public record ExceptionDTO(
-        @Schema(description = "Description of the exception's message", example = "Invalid login or password!")
+        @Schema(description = "Exception's message", example = "Invalid login or password!")
         String message,
 
         @Schema(description = "Exception's HTTP status code as a String", example = "200")
